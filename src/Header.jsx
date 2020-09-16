eda82c2862ab19f536d97e14af9b86c96a5a0648
Anas-Aqeel <71166989+Anas-Aqeel@users.noreply.github.com>
2020-09-16 23:19:56 +0500

    Navbar in Process

---
+++ src/Header.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import './Header.css'
+import { Link } from 'react-router-dom';
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+
+function Header() {
+  return (
+    <nav className="header">
+        {/* Amazon Logo */}
+        <Link to="/">
+            <img
+                className="header__logo"
+                src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                alt=""
+            />
+        </Link>
+
+        {/* Search Option */}
+        <div className="header__search">
+            <input type="text" className="header__searchInput"/>
+            <SearchIcon className="header__searchIcon"/>
+        </div>
+
+        {/* Links */}
+        <div className="header__nav">
+            {/* first link */}
+            <Link to="/login" className="header__link">
+                <div className="header__option">
+                    <span className="header__optionNumberOne">
+                        Hello
+                    </span>
+                    <span className="header__optionNumberTwo">
+                        Sign In
+                    </span>
+                </div>
+            </Link>
+            {/* second link */}
+            <Link to="/login" className="header__link">
+                <div className="header__option">
+                    <span className="header__optionNumberOne">
+                        Returns
+                    </span>
+                    <span className="header__optionNumberTwo">
+                        & Orders
+                    </span>
+                </div>
+            </Link>
+
+            {/* third link */}
+            <Link to="/login" className="header__link">
+                <div className="header__option">
+                    <span className="header__optionNumberOne">
+                        Try
+                    </span>
+                    <span className="header__optionNumberTwo">
+                        Prime
+                    </span>
+                </div>
+            </Link>
+            {/* fourth link */}
+            <Link to="/checkout" className="header__link">
+                <div className="header__optionBasket">
+                    {/* Shopping Basket Icon */}
+                    < ShoppingBasketIcon/>
+                    {/* Number Of Items In The Basket */}
+                    <span>
+                        0
+                    </span>
+                </div>
+            </Link>
+
+
+        </div>
+    </nav>
+  );
+}
+
+export default Header;

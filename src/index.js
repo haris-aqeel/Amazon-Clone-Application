809194bc4d8c729f1e6e59e53cf00292eed6138d
Haris-Aqeel <harisaqeel.2001@gmail.com>
2020-09-18 14:43:42 +0500

    Adding Reducer

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { initialState } from './reducer';
+import reducer , { initialState } from './reducer';
 import StateProvider from './StateProvider';
 
 
 ReactDOM.render(
-    <StateProvider initialState = {initialState} reducer = {} >
+    <StateProvider initialState = {initialState} reducer = {reducer} >
         <App />
     </StateProvider>
     ,document.getElementById('root'));
--- src/reducer.js
+++ src/reducer.js
@@ -12,3 +12,5 @@ function reducer(state, action){
             return state;
     }
 }
+
+export default reducer;

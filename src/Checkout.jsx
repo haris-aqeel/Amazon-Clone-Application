ce32e2cd361c59d0fc18ae5cc13be65cab3fd309
Haris-Aqeel <harisaqeel.2001@gmail.com>
2020-09-18 18:21:23 +0500

    CheckoutProduct in Process

--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useStateValue } from "./StateProvider";
-import './Checkout.css'
+import "./Checkout.css";
 
 const Checkout = () => {
   const [{ basket }, dispatch] = useStateValue();
@@ -12,6 +12,29 @@ const Checkout = () => {
         alt="Advertisement"
         src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
       />
+
+      {basket?.length === 0 ? (
+        <div>
+          <h2>Your Shopping Basket is Empty.</h2>
+          <p>
+            You have no items in your basket. To buy one or more items, click
+            "Add to basket" next to the item.
+          </p>
+        </div>
+      ) : (
+        <div>
+          <h2 className='checkout__title'>Shopping Basket</h2>
+          {basket.map((item)=>{
+              <CheckoutProduct 
+              id={item.id}
+              title={item.title}
+              image = {item.image}
+              price={item.price}
+              rating={item.rating}/>
+          })
+          }
+        </div>
+      )}
     </div>
   );
 };
---
+++ src/CheckoutProduct.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CheckoutProduct = ({id, title, image, price, rating}) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CheckoutProduct
